6b72718e288f3cbe7fb031426981c81607600b5a
김승녕 <workingsnkim@gmail.com>
2024-01-23 16:39:31 +0900

    ValueNotifier

--- lib/screens/explict_animations_screen.dart
+++ lib/screens/explict_animations_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 class ExplictAnimationScreen extends StatefulWidget {
@@ -14,15 +12,44 @@ class _ExplictAnimationScreenState extends State<ExplictAnimationScreen>
   late final AnimationController _animationController = AnimationController(
     vsync: this,
     duration: const Duration(seconds: 2),
+  )..addListener(() {
+      _value.value = _animationController.value;
+    });
+
+  late final Animation<Decoration> _decoration = DecorationTween(
+      begin: BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      end: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(120),
+      )).animate(_curved);
+
+  late final Animation<double> _rotation =
+      Tween(begin: 0.0, end: 0.5).animate(_curved);
+
+  late final Animation<double> _scale =
+      Tween(begin: 1.0, end: 1.3).animate(_curved);
+
+  late final Animation<Offset> _offset =
+      Tween(begin: Offset.zero, end: const Offset(0, -0.2)).animate(_curved);
+
+  late final CurvedAnimation _curved = CurvedAnimation(
+    parent: _animationController,
+    curve: Curves.elasticOut,
+    reverseCurve: Curves.bounceIn,
   );
 
-  late final Animation<Color?> _color = ColorTween(
-    begin: Colors.amber,
-    end: Colors.red,
-  ).animate(_animationController);
   @override
   void initState() {
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   void _play() {
@@ -37,6 +64,13 @@ class _ExplictAnimationScreenState extends State<ExplictAnimationScreen>
     _animationController.reverse();
   }
 
+  final ValueNotifier<double> _value = ValueNotifier(0.0);
+
+  void _onChaange(double value) {
+    _value.value = 0;
+    _animationController.value = value;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,17 +81,20 @@ class _ExplictAnimationScreenState extends State<ExplictAnimationScreen>
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          AnimatedBuilder(
-            animation: _color,
-            builder: (context, child) {
-              return Container(
-                child: Container(
-                  color: _color.value,
-                  width: 400,
-                  height: 400,
-                ),
-              );
-            },
+          SlideTransition(
+            position: _offset,
+            child: RotationTransition(
+              turns: _rotation,
+              child: DecoratedBoxTransition(
+                  decoration: _decoration,
+                  child: const SizedBox(
+                    height: 400,
+                    width: 400,
+                  )),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -75,7 +112,15 @@ class _ExplictAnimationScreenState extends State<ExplictAnimationScreen>
                 child: const Text("rewind"),
               ),
             ],
-          )
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          ValueListenableBuilder(
+              valueListenable: _value,
+              builder: (context, value, child) {
+                return Slider(value: _value.value, onChanged: _onChaange);
+              }),
         ],
       )),
     );
