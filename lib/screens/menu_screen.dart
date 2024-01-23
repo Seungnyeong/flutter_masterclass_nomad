1511397f8d38d06e5977cfee7a296e833cdd71bb
김승녕 <workingsnkim@gmail.com>
2024-01-23 13:21:19 +0900

    Animated Container

---
+++ lib/screens/menu_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations_masterclass/screens/implicit_anmiations_screen.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  void _goToPage(BuildContext context, Widget screen) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Animiations"),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              _goToPage(context, const ImplicitAnimationsScreen());
+            },
+            child: const Text('Implicit Animations'),
+          ),
+        ],
+      )),
+    );
+  }
+}
