d09e6c0f2eeedde640a489188763f5fc1c3ff640
김승녕 <workingsnkim@gmail.com>
2024-02-03 00:27:23 +0900

    Duration Track Bar Anmitaion

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animations_masterclass/screens/apple_watch_screen.dart';
 import 'package:flutter_animations_masterclass/screens/explict_animations_screen.dart';
 import 'package:flutter_animations_masterclass/screens/implicit_anmiations_screen.dart';
+import 'package:flutter_animations_masterclass/screens/music_player_screen.dart';
 import 'package:flutter_animations_masterclass/screens/swiping_cards_screen.dart';
 
 class MenuScreen extends StatelessWidget {
@@ -43,6 +44,12 @@ class MenuScreen extends StatelessWidget {
               _goToPage(context, const SwipingCardsScreen());
             },
             child: const Text('Swiping Cards Screen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _goToPage(context, const MusicPlayerScreen());
+            },
+            child: const Text('Music Player Screen'),
           ),
         ],
       )),
---
+++ lib/screens/music_player_detail_screen.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerDetailScreen extends StatefulWidget {
+  final int index;
+
+  const MusicPlayerDetailScreen({super.key, required this.index});
+
+  @override
+  State<MusicPlayerDetailScreen> createState() =>
+      _MusicPlayerDetailScreenState();
+}
+
+class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _progressController =
+      AnimationController(vsync: this, duration: const Duration(minutes: 1))
+        ..repeat(
+          reverse: true,
+        );
+
+  @override
+  void dispose() {
+    _progressController.dispose();
+    super.dispose();
+  }
+
+  String toTimeString(double value) {
+    final duration = Duration(milliseconds: (value * 60000).toInt());
+    final timeString =
+        '${duration.inMinutes.toString().padLeft(2, '0')}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
+    return timeString;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Interstellar"),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Hero(
+              tag: "${widget.index}",
+              child: Container(
+                height: 350,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.4),
+                      blurRadius: 20,
+                      spreadRadius: 2,
+                      offset: const Offset(0, 8),
+                    )
+                  ],
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/covers/${widget.index}.jpeg")),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          AnimatedBuilder(
+            animation: _progressController,
+            builder: (context, child) {
+              return CustomPaint(
+                painter: ProgressBar(
+                  progressValue: _progressController.value,
+                ),
+                size: Size(
+                  size.width - 80,
+                  5,
+                ),
+              );
+            },
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          AnimatedBuilder(
+            animation: _progressController,
+            builder: (context, child) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Row(
+                  children: [
+                    Text(
+                      toTimeString(_progressController.value),
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    const Spacer(),
+                    Text(
+                      toTimeString(1 - _progressController.value),
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text("Interstellar",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
+          const SizedBox(
+            height: 5,
+          ),
+          const Text(
+            "A Film By Christopher NOlan - Original Motion Picture Sound Track",
+            maxLines: 1,
+            overflow: TextOverflow.visible,
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProgressBar extends CustomPainter {
+  final double progressValue;
+  ProgressBar({required this.progressValue});
+  @override
+  void paint(Canvas canvas, Size size) {
+    final progress = size.width * progressValue;
+    // track
+    final trackPaint = Paint()
+      ..color = Colors.grey.shade300
+      ..style = PaintingStyle.fill;
+
+    final trackRRect = RRect.fromLTRBR(
+      0,
+      0,
+      size.width,
+      size.height,
+      const Radius.circular(10),
+    );
+
+    canvas.drawRRect(trackRRect, trackPaint);
+
+    // progress
+    final progressPaint = Paint()
+      ..color = Colors.grey.shade500
+      ..style = PaintingStyle.fill;
+
+    final progressRRect =
+        RRect.fromLTRBR(0, 0, progress, size.height, const Radius.circular(10));
+
+    canvas.drawRRect(progressRRect, progressPaint);
+
+    // thumb
+    canvas.drawCircle(Offset(progress, size.height / 2), 10, progressPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant ProgressBar oldDelegate) {
+    return oldDelegate.progressValue != progressValue;
+  }
+}
