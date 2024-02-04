258acba336967d58514440ea4ed663f1b7040766
김승녕 <workingsnkim@gmail.com>
2024-02-04 20:48:24 +0900

    Volume Container

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class MusicPlayerDetailScreen extends StatefulWidget {
   final int index;
@@ -11,16 +12,33 @@ class MusicPlayerDetailScreen extends StatefulWidget {
 }
 
 class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late final AnimationController _progressController =
       AnimationController(vsync: this, duration: const Duration(minutes: 1))
         ..repeat(
           reverse: true,
         );
 
+  late final AnimationController _playPauseContoller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 500),
+  );
+
+  late final AnimationController _marqueeController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 20),
+  )..repeat(reverse: true);
+
+  late final Animation<Offset> _marqueeTween = Tween(
+    begin: const Offset(0.1, 0),
+    end: const Offset(-0.6, 0),
+  ).animate(_marqueeController);
+
   @override
   void dispose() {
     _progressController.dispose();
+    _marqueeController.dispose();
+    _playPauseContoller.dispose();
     super.dispose();
   }
 
@@ -31,12 +49,43 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
     return timeString;
   }
 
+  void _onPlayPauseTap() {
+    if (_playPauseContoller.isCompleted) {
+      _playPauseContoller.reverse();
+    } else {
+      _playPauseContoller.forward();
+    }
+  }
+
+  bool _dragging = false;
+
+  void _toggleDragging() {
+    setState(() {
+      _dragging = !_dragging;
+    });
+  }
+
+  final ValueNotifier<double> _volume = ValueNotifier(0);
+  late final size = MediaQuery.of(context).size;
+
+  void _onVolumeDragUpdate(DragUpdateDetails details) {
+    _volume.value += details.delta.dx;
+    _volume.value = _volume.value.clamp(
+      0.0,
+      size.width - 80,
+    );
+  }
+
+  void _openMenu() {}
+
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Interstellar"),
+        actions: [
+          IconButton(onPressed: _openMenu, icon: const Icon(Icons.menu))
+        ],
       ),
       body: Column(
         children: [
@@ -122,12 +171,72 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
           const SizedBox(
             height: 5,
           ),
-          const Text(
-            "A Film By Christopher NOlan - Original Motion Picture Sound Track",
-            maxLines: 1,
-            overflow: TextOverflow.visible,
-            style: TextStyle(
-              fontSize: 18,
+          SlideTransition(
+            position: _marqueeTween,
+            child: const Text(
+              "A Film By Christopher Nolan - Original Motion Picture Sound Track",
+              maxLines: 1,
+              softWrap: false,
+              overflow: TextOverflow.visible,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+            onTap: _onPlayPauseTap,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AnimatedIcon(
+                  icon: AnimatedIcons.pause_play,
+                  progress: _playPauseContoller,
+                  size: 60,
+                ),
+                // 로티를 사용하여 애니메이션을 사서 사용할 수 있다.
+                // LottieBuilder.asset(
+                //   "assets/animations/play-lottie.json",
+                //   controller: _playPauseContoller,
+                //   onLoaded: (composition) {
+                //     _playPauseContoller.duration = composition.duration;
+                //   },
+                //   width: 200,
+                //   height: 200,
+                // ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+            onHorizontalDragUpdate: _onVolumeDragUpdate,
+            onHorizontalDragStart: (_) => _toggleDragging(),
+            onHorizontalDragEnd: (_) => _toggleDragging(),
+            child: AnimatedScale(
+              scale: _dragging ? 1.1 : 1,
+              duration: const Duration(
+                milliseconds: 500,
+              ),
+              curve: Curves.bounceOut,
+              child: Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ValueListenableBuilder(
+                  valueListenable: _volume,
+                  builder: (context, value, child) => CustomPaint(
+                    size: Size(size.width - 80, 50),
+                    painter: VolumPaint(
+                      volume: value,
+                    ),
+                  ),
+                ),
+              ),
             ),
           )
         ],
@@ -174,5 +283,28 @@ class ProgressBar extends CustomPainter {
   @override
   bool shouldRepaint(covariant ProgressBar oldDelegate) {
     return oldDelegate.progressValue != progressValue;
+  }
+}
+
+class VolumPaint extends CustomPainter {
+  final double volume;
+  VolumPaint({
+    required this.volume,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final bgPaint = Paint()..color = Colors.grey.shade300;
+    final bgRect = Rect.fromLTWH(0, 0, size.width, size.height);
+    canvas.drawRect(bgRect, bgPaint);
+
+    final volumePaint = Paint()..color = Colors.grey.shade500;
+    final volumRect = Rect.fromLTRB(0, 0, volume, size.height);
+    canvas.drawRect(volumRect, volumePaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant VolumPaint oldDelegate) {
+    return oldDelegate.volume != volume;
   }
 }
