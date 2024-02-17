25cfef6169edab27f1f7c7040a53cb9a23182276
김승녕 <workingsnkim@gmail.com>
2024-02-17 15:52:40 +0900

    Card filp

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -8,6 +8,7 @@ import 'package:flutter_animations_masterclass/screens/music_player_screen.dart'
 import 'package:flutter_animations_masterclass/screens/rive_screen.dart';
 import 'package:flutter_animations_masterclass/screens/shared_axis_screen.dart';
 import 'package:flutter_animations_masterclass/screens/swiping_cards_screen.dart';
+import 'package:flutter_animations_masterclass/screens/wallet_screen.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
@@ -78,6 +79,12 @@ class MenuScreen extends StatelessWidget {
               _goToPage(context, const FadeThroughScreen());
             },
             child: const Text('Fade Through'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _goToPage(context, const WalletScreenState());
+            },
+            child: const Text('Wallet Screen'),
           ),
         ],
       )),
